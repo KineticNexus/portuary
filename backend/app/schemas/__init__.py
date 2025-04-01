f0f1fc5f783e96f2d0536209052e334c526663da
Kinetic Nexus <86033455+KineticNexus@users.noreply.github.com>
2025-04-01 20:10:18 -0300

    Add schemas package initialization

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .user import User, UserCreate, UserUpdate, UserInDB
+from .vessel import Vessel, VesselCreate, VesselUpdate, VesselNested
+from .cargo import Cargo, CargoCreate, CargoUpdate, CargoNested
+from .dock import Dock, DockCreate, DockUpdate, DockNested
+from .schedule import Schedule, ScheduleCreate, ScheduleUpdate, ScheduleDetail, ScheduleNested
